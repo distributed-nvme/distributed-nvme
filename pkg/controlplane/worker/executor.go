f85d604f0f8515edbd4eb7c0ab3d83f5fa59cee0
yupeng <yupeng0921@gmail.com>
2024-08-10 02:03:43 +0000

    workers lisen on grpc

--- pkg/controlplane/worker/executor.go
+++ pkg/controlplane/worker/executor.go
@@ -2,20 +2,21 @@ package worker
 
 import (
 	"context"
-	"time"
-	// "net"
+	"net"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 
-	// "google.golang.org/grpc"
 	"github.com/spf13/cobra"
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/grpc"
 
 	"github.com/distributed-nvme/distributed-nvme/pkg/lib/constants"
+	"github.com/distributed-nvme/distributed-nvme/pkg/lib/ctxhelper"
 	"github.com/distributed-nvme/distributed-nvme/pkg/lib/prefixlog"
-	// pbcp "github.com/distributed-nvme/distributed-nvme/pkg/proto/controlplane"
+	pbcp "github.com/distributed-nvme/distributed-nvme/pkg/proto/controlplane"
 )
 
 type workerArgsStruct struct {
@@ -73,6 +74,17 @@ func init() {
 func launchWorker(cmd *cobra.Command, args []string) {
 	gLogger.Info("Launch worker: %v", workerArgs)
 
+	lis, err := net.Listen(workerArgs.grpcNetwork, workerArgs.grpcAddress)
+	if err != nil {
+		gLogger.Fatal("Listen err: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			ctxhelper.UnaryServerPerCtxHelperInterceptor,
+		),
+	)
+
 	prioCode, err := initPrioCode(workerArgs.prioCodeConf)
 	if err != nil {
 		gLogger.Fatal("Init prio code err: %v", err)
@@ -87,36 +99,63 @@ func launchWorker(cmd *cobra.Command, args []string) {
 	if err != nil {
 		gLogger.Fatal("Create etcd client err: %v", err)
 	}
-	var worker workerI
+
+	var mwkr *memberWorker
+	ctx, cancel := context.WithCancel(context.Background())
+
 	switch workerArgs.role {
 	case "dn":
-		worker = newDnWorkerServer(
+		worker := newDnWorkerServer(
 			etcdCli,
 			constants.SchemaPrefixDefault,
+		)
+		pbcp.RegisterDiskNodeWorkerServer(grpcServer, worker)
+		mwkr = newMemberWorker(
+			ctx,
+			workerArgs.grpcTarget,
+			prioCode,
+			uint32(workerArgs.replica),
+			constants.GrantTTLDefault,
+			worker,
 		)
 	case "cn":
-		worker = newCnWorkerServer(
+		worker := newCnWorkerServer(
 			etcdCli,
 			constants.SchemaPrefixDefault,
 		)
+		pbcp.RegisterControllerNodeWorkerServer(grpcServer, worker)
+		mwkr = newMemberWorker(
+			ctx,
+			workerArgs.grpcTarget,
+			prioCode,
+			uint32(workerArgs.replica),
+			constants.GrantTTLDefault,
+			worker,
+		)
 	case "sp":
-		worker = newSpWorkerServer(
+		worker := newSpWorkerServer(
 			etcdCli,
 			constants.SchemaPrefixDefault,
+		)
+		pbcp.RegisterStoragePoolWorkerServer(grpcServer, worker)
+		mwkr = newMemberWorker(
+			ctx,
+			workerArgs.grpcTarget,
+			prioCode,
+			uint32(workerArgs.replica),
+			constants.GrantTTLDefault,
+			worker,
 		)
 	default:
 		gLogger.Fatal("Unknown role: %s", workerArgs.role)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	mwkr := newMemberWorker(
-		ctx,
-		workerArgs.grpcTarget,
-		prioCode,
-		uint32(workerArgs.replica),
-		constants.GrantTTLDefault,
-		worker,
-	)
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			gLogger.Fatal("Serve err: %v", err)
+		}
+	}()
+
 	mwkr.run()
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
